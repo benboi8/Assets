6b66bef29fbac79cb4c309ceba11e6e466adc678
ben williams <45974327+benboi8@users.noreply.github.com>
2022-03-07 17:52:03 +0000

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -246,8 +246,8 @@ def MoveRectWithoutCenter(startPos, startRect):
 
 
 def DrawRectAlpha(surface, color, rect):
-    shape_surf = pygame.Surface(pygame.Rect(rect).size, pygame.SRCALPHA)
-    pygame.draw.rect(shape_surf, color, shape_surf.get_rect())
+    shape_surf = pg.Surface(pg.Rect(rect).size, pg.SRCALPHA)
+    pg.draw.rect(shape_surf, color, shape_surf.get_rect())
     surface.blit(shape_surf, rect)
 
 
@@ -261,9 +261,9 @@ def DrawCircleAlpha(surface, color, center, radius):
 def DrawPolygonAlpha(surface, color, points):
     lx, ly = zip(*points)
     min_x, min_y, max_x, max_y = min(lx), min(ly), max(lx), max(ly)
-    target_rect = pygame.Rect(min_x, min_y, max_x - min_x, max_y - min_y)
-    shape_surf = pygame.Surface(target_rect.size, pygame.SRCALPHA)
-    pygame.draw.polygon(shape_surf, color, [(x - min_x, y - min_y) for x, y in points])
+    target_rect = pg.Rect(min_x, min_y, max_x - min_x, max_y - min_y)
+    shape_surf = pg.Surface(target_rect.size, pg.SRCALPHA)
+    pg.draw.polygon(shape_surf, color, [(x - min_x, y - min_y) for x, y in points])
     surface.blit(shape_surf, target_rect)
 
 
